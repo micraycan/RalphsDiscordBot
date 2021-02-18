aa34db2aa1994570754a3f5ef0169abf0dc86a45
Michael <micraycan@gmail.com>
2021-02-17 19:56:20 -0500

    setup

---
+++ Bot.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RalphsDiscordBot
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+        }
+    }
+}
